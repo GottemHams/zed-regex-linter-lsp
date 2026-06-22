1525e7b0448b0100334ba7bb1b404d11db4df392
Wazakindjes <waza@malvager.net>
2026-06-22 20:47:23 +0200

    Use v-prefixed release tags

--- src/extension.rs
+++ src/extension.rs
@@ -91,7 +91,7 @@ impl RegexLinterLspExtension {
 		};
 
 		let archive_name = format!("{}.{}", binary_name, archive_ext);
-		let release_url = format!("{}/{}/{}", RELEASE_BASE_URL, VERSION, archive_name);
+		let release_url = format!("{}/v{}/{}", RELEASE_BASE_URL, VERSION, archive_name);
 		zed::download_file(&release_url, &working_dir, archive_type)
 			.map_err(|e| format!("Failed to download binary from release: {}", e))?;
 
